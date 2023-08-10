d4544b9f2a895aff1bcd24dfb3aed4bec53e7fa5
Eric <ericfrancis1@proton.me>
2023-08-09 21:46:03 -0500

    clk.js testing

--- functions/clk.js
+++ functions/clk.js
@@ -13,67 +13,72 @@ const utils = require('../utils/utils');
 
 
 exports.handler = async function (event, context) {
-    const cookies = utils.makeCookieObj(event.headers.cookie || '');
+    // const cookies = utils.makeCookieObj(event.headers.cookie || '');
 
-    let click_id = cookies.click_id;
-    if (!click_id) click_id = event.queryStringParameters.click_id;
+    // let click_id = cookies.click_id;
+    // if (!click_id) click_id = event.queryStringParameters.click_id;
 
-    let campaign_id = cookies.campaign_id;
+    // let campaign_id = cookies.campaign_id;
 
-    try {
-        if (!campaign_id) {
-            // query database for this click_id, and return the campaign_id associated with it
+    // try {
+    //     if (!campaign_id) {
+    //         // query database for this click_id, and return the campaign_id associated with it
 
-            // campaign_id = ...
-        }
+    //         // campaign_id = ...
+    //     }
 
-        const campaign = require(`../data/campaigns/${campaign_id}.json`);
+    //     const campaign = require(`../data/campaigns/${campaign_id}.json`);
 
-        let targetOffer;
+    //     let targetOffer;
 
-        const isRandomRotation = campaign.offerRotation === strings.rotations.random;
-        if (isRandomRotation) {
-            const numOffers = campaign.flow[2].dataArr.length;
-            const randomIndex = utils.randomIntBetween(0, numOffers - 1);
-            targetOffer = campaign.flow[2].dataArr[randomIndex];
-        } else {
-            return {
-                statusCode: 302,
-                headers: {
-                    'Location': settings.default.smartLink
-                },
-                body: ''
-            }
-        }
+    //     const isRandomRotation = campaign.offerRotation === strings.rotations.random;
+    //     if (isRandomRotation) {
+    //         const numOffers = campaign.flow[2].dataArr.length;
+    //         const randomIndex = utils.randomIntBetween(0, numOffers - 1);
+    //         targetOffer = campaign.flow[2].dataArr[randomIndex];
+    //     } else {
+    //         return {
+    //             statusCode: 302,
+    //             headers: {
+    //                 'Location': settings.default.smartLink
+    //             },
+    //             body: ''
+    //         }
+    //     }
 
-        async function updateClick() {
-            await dbConnection;
-            const click = await Click.findOne({ click_id: click_id });
-            console.log(click);
+    //     async function updateClick() {
+    //         await dbConnection;
+    //         const click = await Click.findOne({ click_id: click_id });
+    //         console.log(click);
 
-            click.offerClicked = true;
-            click.offer = targetOffer;
+    //         click.offerClicked = true;
+    //         click.offer = targetOffer;
 
-            await click.save();
-            console.log('Saved click to DB');
-        }
-        updateClick();
+    //         await click.save();
+    //         console.log('Saved click to DB');
+    //     }
+    //     updateClick();
 
-        return {
-            statusCode: 302,
-            headers: {
-                'Location': targetOffer.url
-            },
-            body: '',
-        }
-    } catch (err) {
-        console.error(err);
-        return {
-            statusCode: 302,
-            headers: {
-                'Location': settings.default.smartLink
-            },
-            body: ''
-        }
+    //     return {
+    //         statusCode: 302,
+    //         headers: {
+    //             'Location': targetOffer.url
+    //         },
+    //         body: '',
+    //     }
+    // } catch (err) {
+    //     console.error(err);
+    //     return {
+    //         statusCode: 302,
+    //         headers: {
+    //             'Location': settings.default.smartLink
+    //         },
+    //         body: ''
+    //     }
+    // }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: '/clk' })
     }
 }
