c79acb0c25bbb9ca21cbddf3806cd14e88fe9acf
Eric <ericfrancis1@proton.me>
2023-08-10 16:10:42 -0500

    experimenting

--- functions/test.js
+++ functions/test.js
@@ -8,16 +8,16 @@ const Click = require('../models/Click');
 
 exports.handler = async function (event, context) {
     console.log('Loading test.json file now');
-    const testData = require(`./data/campaigns/1689103366291.json`);
+    const testData = require(`./data/campaigns/test.json`);
 
     await dbConnection;
     const clicks = await Click.find({});
 
-    // const campaignPath = `./data/campaigns/1689103366291.json`;
-    // const campaign = require(campaignPath);
+    const campaignPath = `./data/campaigns/1689103366291.json`;
+    const campaign = require(campaignPath);
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: '/test', testdata: testData, event: event, clicks: clicks }, null, 4)
+        body: JSON.stringify({ message: '/test', testdata: testData, campaign: campaign, event: event, clicks: clicks }, null, 4)
     }
 }
