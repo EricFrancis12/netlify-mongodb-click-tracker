e320ee53fc9411c905792f2db68a6cddd24b9832
Eric <ericfrancis1@proton.me>
2023-08-10 15:57:54 -0500

    experimenting

--- functions/test.js
+++ functions/test.js
@@ -8,7 +8,7 @@ const Click = require('../models/Click');
 
 exports.handler = async function (event, context) {
     console.log('Loading test.json file now');
-    const testData = require('./data/test.json');
+    const testData = require('./data/campaigns/test.json');
 
     await dbConnection;
     const clicks = await Click.find({});
